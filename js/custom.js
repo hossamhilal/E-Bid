8d64449c834a3a2c4f73923f36fee65285fd55dd
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2020-09-19 03:37:18 +0300

    [ cart - product - login -signUp ]  pages

--- js/custom.js
+++ js/custom.js
@@ -155,28 +155,138 @@ $(document).ready(function ($) {
         }
     });
 
+    // Add To Favourite 
     $('.addToFav').on('click', function () {
         $(this).find('i').toggleClass('far').toggleClass('fas');     
     });
 
-    // INPUT ANIMATION 
-    // $('.filedInput').each(function() { 
-    //     if ($(this).val() != "") {
-    //         $(this).parent('.field').addClass('focused');   
+    // Toggle Password 
+    $('.showPassword').click(function(){
+        let passWord = $(this).parent().find('input');
+        if($(passWord).attr('type') == 'password'){
+             $(passWord).prop('type', 'text');
+        }else{
+             $(passWord).prop('type', 'password');
+        }
+    });
+
+    // Filter Collapse
+    $('.toggleCollapse').on('click', function () {
+        $(this).parent().next('.filterCollapseBody').slideToggle(); 
+        $(this).find('i').toggleClass('icofont-minus').toggleClass('icofont-plus'); 
+    });
+
+    // Mazad Count
+    $('.mountBtn').on('click' , function(){
+        let startValue = parseInt($('.mountNumber').text());
+        let CountBox = $(this).parent().find('.mountNumber');
+        let currentNumber = 1 ;
+        
+        if ($(this).hasClass('plusBtn')) {
+            CountBox.text(startValue + currentNumber);
+        } else {
+            if(startValue > 0) CountBox.text(startValue - currentNumber);    
+        }
+    });
+
+    // Thumbs OWL 
+    $('.owlThumbs').owlCarousel({
+        rtl: true ,
+        margin: 10,
+        autoplay: false,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplaySpeed : 1000,
+        autoplayTimeout : 1000,
+        smartSpeed: 1000 ,
+        navText: ["<i class='icofont-thin-right'></i>", "<i class='icofont-thin-left'></i>"],
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    // Preview Active Image in Ad Slider Thumbs 
+    $('#activeSlid img').attr('src' , $('.owlThumbs .owl-item.active.center img').attr('src'));
+
+    let imagesOwl = $('.owlThumbs');
+    imagesOwl.on('changed.owl.carousel', function(e) {
+        let ActiveSrc = $('.owlThumbs .owl-item.active.center img').attr('src');
+        $('#activeSlid img').attr('src', ActiveSrc);
+    });
+
+    $(document).on('click','.owlThumbs .owl-prev , .owlThumbs .owl-next', function(){
+        let ActiveSrc = $('.owlThumbs .owl-item.active.center img').attr('src');
+        $('#activeSlid img').attr('src', ActiveSrc);
+    });
+
+    $(document).on('click','.owlThumbs .item', function(){
+        let ActiveSrc = $(this).find('img').attr('src');
+        $('#activeSlid img').attr('src', ActiveSrc);
+    });
+
+    // Tabs List
+    $('.tabBtn').on('click' , function(e){
+        e.preventDefault();
+        $('.tabBtn').removeClass('active');
+        $(this).addClass('active');
+        
+        var itemId = $(this).attr("href"); 
+        $('.tabContent').removeClass('show'); 
+        $(itemId).addClass('show');
+    });
+
+
+    // Range Slider 
+    $('#rangeSlider').slider({
+        range: true,
+        isRTL: true,
+        min: 0,
+        max: 1000,
+        values: [ 0, 500 ],
+        slide: function( event, ui ) {
+            $('.minRange').text(" SR " + ui.values[0]);
+            $('.maxRange').text(" SR " + ui.values[1]);
+        }
+    });
+
+    $('.minRange').text(" SR " + $('#rangeSlider').slider( "values", 0 ));
+    $('.maxRange').text(" SR " + $('#rangeSlider').slider( "values", 1 ));
+
+    $('.rangeValues .min').text($('#rangeSlider').slider("option", "min") + " SR");
+    $('.rangeValues .max').text($('#rangeSlider').slider("option", "max") + " SR");
+
+
+    
+
+
+    // Upload Avatar 
+    // function uploadAvatar(input , place) {
+    //     if (input.files && input.files[0]) {
+    //         let reader = new FileReader();
+    //         reader.onload = function(e) {
+    //             let preview = place;
+    //             let newSrc = e.target.result;
+    //             $(preview).find('img').attr('src' , newSrc );
+    //         }
+    //         reader.readAsDataURL(input.files[0]);
     //     }
+    // }
+    // $('.uploadAvatar input').change(function() {
+    //     uploadAvatar(this , $(this).parent().prev('.profileAvatarPreview'));
     // });
-
-    // $('.filedInput').focus(function(){
-    //     $(this).parent().addClass('focused');
-    // });
-
-    // $('.filedInput').focusout(function(){
-    //     if($(this).val() === "")
-    //     $(this).parent('.field').removeClass('focused');
-    // });
+  
 
     // iniat WOW Js
-    new WOW().init();
+    // new WOW().init();
 
 });
 
